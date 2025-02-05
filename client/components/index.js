3a206167692dfb2378a7fe595307193fef4f90c8
ernraff <103540977+ernraff@users.noreply.github.com>
2025-02-04 22:51:51 -0800

    resolve merge conflicts

--- client/components/index.js
+++ client/components/index.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import Chat from "./Chat"
 import Inputs from "./Inputs"
 import SignUp from "./SignUp"
 
-=======
-import Chat from "./Chat"
-import Inputs from "./Inputs"
-import SignUp from "./SignUp"
-
->>>>>>> b98710cd950c695db1b6f15f194e74cfbc6accd5
 export { Chat, Inputs, SignUp }
--- client/postcss.config.mjs
+++ client/postcss.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
@@ -7,13 +6,3 @@ const config = {
 };
 
 export default config;
-=======
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
-
-export default config;
->>>>>>> b98710cd950c695db1b6f15f194e74cfbc6accd5
